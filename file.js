d7db664d444e08093fee68ae1ed3a9bed5e4c6d8
Vittoria <vitto.tonini@gmail.com>
2025-12-17 09:50:31 +0100

    added options array

--- file.js
+++ file.js
@@ -1,2 +1,30 @@
 const today = new Date();
 console.log(today);
+
+const options = [
+  {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    weekday: "long",
+  },
+  {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+  },
+  {
+    year: "numeric",
+    month: "2-digit",
+    day: "short",
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  }
+]
+
+console.log(today.toLocaleString(undefined, options[0]));
+console.log(today.toLocaleString(undefined, options[1]));
+console.log(today.toLocaleString(undefined, options[2]));
